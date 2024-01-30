b76fc92eeecbab25900082c79828cef42498da59
Роман <romanzadorozhniy92@gmail.com>
2024-01-30 21:26:43 +0300

    Final
    Final

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
+    var audio = $('#audio-track')[0]
 
+    $playBtn = $('#audio-play')
+    $pauseBtn = $('#audio-pause')
+    $audioControl = $('[data-type="audio-control"]')
 
+    setTimeout(initAudioPlayer, 1500)
+
+    function initAudioPlayer() {
+        $playBtn.parent().show()
+        audio.loop = true
+        audio.play()
+
+        $audioControl.click(function () {
+            if(audio.paused) {
+                audio.play()
+                $playBtn.parent().hide()
+                $pauseBtn.parent().show()
+            } else {
+                audio.pause()
+                $playBtn.parent().show()
+                $pauseBtn.parent().hide()
+            }
+        })
+    }
 });
